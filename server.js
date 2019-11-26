9289f60d7a84f83b37a4c0d5fc6ded4720ee7077
Daniele Esposito <esp.dne@gmail.com>
2019-11-26 19:51:22 +0100

    arrays

--- server.js
+++ server.js
@@ -9,20 +9,36 @@ var app = express();
 var PORT = 4000;
 
 
-// get homepage
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+
+// array for reservation
+
+let reservations = new Array(5);  /// new Array(5); array with a limit of 5 elements
+
+//array for waiting list
+
+let waitingList = [];
+
+/********************************************************* */
+// get homepage
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
+// get reserve
 app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+// get tables
 app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "tables.html"));
 });
 
+//**************************************************************** */
 
 // Listening the PORT
 // =============================================================
