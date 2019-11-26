2bcf306a791d85035f6b7f77d010bdfeb0c08e08
Daniele Esposito <esp.dne@gmail.com>
2019-11-26 19:33:01 +0100

    allhtml

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ var app = express();
 var PORT = 4000;
 
 
+// get homepage
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function (req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+});
 
 
 // Listening the PORT
